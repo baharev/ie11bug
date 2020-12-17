6d7a251f85cf792170701a4d6f875899bc907162
baharev <ali.baharev@gmail.com>
2020-12-17 16:03:06 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <p>Hello, World!</p>,
+    document.getElementById('root')
+);
